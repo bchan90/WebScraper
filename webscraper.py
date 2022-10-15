02931f7cf77a726460654415b9e77d82759ab09b
Kali <bchan90@gmail.com>
2022-10-15 02:55:49 -0400

    v.2

--- webscraper.py
+++ webscraper.py
@@ -2,8 +2,12 @@
 
 import sys
 import re
+import requests
 from urllib.parse import urlsplit
 from urllib.request import Request, urlopen
+from collections import deque
+from bs4 import BeautifulSoup
+import pandas as pd
 
 def check_static(str_url):
     req = Request(
@@ -20,7 +24,56 @@ def check_static(str_url):
         return 0
 
 def web_scraper(str_url):
-    url = str_url
+    original_url = str_url
+
+    unscraped = deque([original_url])
+    scraped = set()
+    emails = set()
+
+    num_to_scrape = 50
+
+    while len(unscraped) and len(scraped) < num_to_scrape:
+        url = unscraped.popleft()
+        scraped.add(url)
+
+        parts = urlsplit(url)
+
+        base_url = f'{parts[0]}://{parts[1]}'
+        if '/' in parts[2]:
+            path = url[:url.rfind('/') + 1]
+        else:
+            path = url
+
+        print(f'Crawling URL {url}')
+        try:
+            response = requests.get(url)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            print('Excpetion caught')
+            continue
+
+        new_emails = set(re.findall(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.co', response.text, re.I))
+        emails.update(new_emails)
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        for anchor in soup.find_all('a'):
+            if 'href' in anchor.attrs:
+                link = anchor.attrs['href']
+            else:
+                link = ''
+
+            if link.startswith('/'):
+                link = base_url + link
+            elif not link.startswith('http'):
+                link = path + link
+
+            if not link.endswith('.gz'):
+                if link.startswith(original_url):
+                    if not link in unscraped and not link in scraped:
+                        unscraped.append(link)
+
+    df = pd.DataFrame(emails, columns = ['Email'])
+    df.to_csv('email.csv', index = False)
 
 def main():
     ## implement argparser ##
